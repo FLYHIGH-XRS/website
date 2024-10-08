99a718c0cee08a2a5684997ecb2064252c844f28
kKsk <alonzomax1210@gmail.com>
2024-10-08 21:14:52 +0800

    Update TimeAttack.vue

--- src/views/ranking/TimeAttack.vue
+++ src/views/ranking/TimeAttack.vue
@@ -46,9 +46,17 @@
         </div>
     </div>
 
+    <div v-if="leaderboard && leaderboard.length > 0">
+        <p class="form-text" id="dataFetchTimeText" style="margin-top: 0;">数据获取时间：{{ this.leaderboard[0].fetchTime }}
+        </p>
+        <p class="form-text" id="fetchInfoText" style="margin-top: -20px;">※ 每10分钟获取一次数据 | 每页显示{{ this.itemsPerPage
+            }}名次数据</p>
+    </div>
+    <p class="form-text" id="loadingText" style="font-size: large;" hidden>加载数据中......请稍候</p>
+
     <!-- 分页 -->
     <nav aria-label="Page navigation" v-if="totalPages() > 1" class="d-flex justify-content-end"
-        style="margin-top: 10px;">
+        style="margin-bottom: -20px; margin-top: -25px;">
         <ul class="pagination justify-content-center" style="list-style: none;">
             <li class="page-item page-item-vp-fix" :class="{ disabled: currentPage === 1 }">
                 <a class="page-link page-link-vp-fix" href="#" @click.prevent="changePage(1)">&laquo;</a>
@@ -69,7 +77,7 @@
     </nav>
 
     <!-- 电脑端布局 -->
-    <table class="table table-striped table-hover pc_table" style="width: 100%; table-layout: fixed;">
+    <table class="table table-striped table-hover pc_table" id="pc_table" style="width: 100%; table-layout: fixed;">
         <thead>
             <tr class="table-header">
                 <th scope="col">排名</th>
@@ -82,7 +90,7 @@
         </thead>
         <tbody>
             <tr class="table-row" style="cursor: pointer;" v-for="(entry, index) in getPaginatedData()" :key="index"
-                title="点击查看此项详细数据">
+                title="点击查看此项详细数据" @click="this.modalData = entry" data-bs-target="#detailModal" data-bs-toggle="modal">
                 <td :class="getRowClass(index)">
                     <div style="font-weight: bolder;">{{ index + 1 + (currentPage - 1) * itemsPerPage }}</div>
                 </td>
@@ -107,10 +115,11 @@
     </table>
 
     <!-- 手机端布局 -->
-    <table class="table table-striped table-hover mobile_table" style="width: 100%; table-layout: fixed;">
+    <table class="table table-striped table-hover mobile_table" id="mobile_table"
+        style="width: 100%; table-layout: fixed;">
         <tbody>
             <tr class="rankingBody mobile_table_body" v-for="(entry, index) in getPaginatedData()" :key="index"
-                title="点击查看此项详细数据">
+                title="点击查看此项详细数据" @click="this.modalData = entry" data-bs-target="#detailModal" data-bs-toggle="modal">
                 <th class="rank form-text" :class="getRowClass(index)">{{ index + 1 + (currentPage - 1) * itemsPerPage
                     }}</th>
                 <td class="name form-text">
@@ -135,6 +144,54 @@
         </tbody>
     </table>
 
+    <!-- 详细数据 -->
+    <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="detailModalLabel">详细数据</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="card" style="width: 100%;">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-size: larger; font-weight:">{{
+                                this.modalData.car.name }}</h5>
+                            <p class="card-text" style="margin-top: -5px; margin-bottom: -10px;">{{
+                                this.modalData.carName }}</p>
+                        </div>
+                        <ul class="list-group list-group-flush lg-vp-ul-fix">
+                            <li class="list-group-item lg-vp-li-fix">游玩时间：{{ this.modalData.playedAtTime }}</li>
+                            <li class="list-group-item lg-vp-li-fix">总用时：{{ this.modalData.totalTime }}</li>
+                            <li class="list-group-item lg-vp-li-fix">行走区域：{{ this.modalData.racingCourse }}</li>
+                            <li class="list-group-item lg-vp-li-fix">操控等级：{{ this.modalData.controlLevel }}</li>
+                        </ul>
+                    </div>
+                    <div class="card" style="width: 100%; margin-top: 10px;">
+                        <div class="card-header">
+                            分段用时
+                        </div>
+                        <ul class="list-group list-group-flush lg-vp-ul-fix">
+                            <li class="list-group-item lg-vp-li-fix">CP1: {{ this.modalData.sec1Time }}</li>
+                            <li class="list-group-item lg-vp-li-fix">CP2: {{ this.modalData.sec2Time }}</li>
+                            <li class="list-group-item lg-vp-li-fix">CP3: {{ this.modalData.sec3Time }}</li>
+                            <li class="list-group-item lg-vp-li-fix">CP4: {{ this.modalData.sec4Time }}</li>
+                            <li v-if="Number(modalData.sec5Time) !== 0" class="list-group-item">CP5: {{
+                                modalData.sec5Time }}</li>
+                            <li v-if="Number(modalData.sec6Time) !== 0" class="list-group-item">CP6: {{
+                                modalData.sec6Time }}</li>
+                            <li v-if="Number(modalData.sec7Time) !== 0" class="list-group-item">CP7: {{
+                                modalData.sec7Time }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">关闭</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -150,35 +207,58 @@ export default {
             itemsPerPage: 20, // 每页条数
             currentPage: 1, // 当前页
             picsPath: 'https://pics-a1c.pages.dev', // 图片URL
+            modalData: {
+                car: {
+                    name: null,
+                },
+                carName: null,
+                totalTime: null,
+                controlLevel: null,
+            }, // 用于存储模态框数据
         }
     },
     methods: {
         getTimeAttackResult() {
+            this.leaderboard = [];
+            // 禁用下拉框
+            document.getElementById('floatingSelectGrid_Course').setAttribute('disabled', 'disabled');
+            document.getElementById('floatingSelectGrid_CarModel').setAttribute('disabled', 'disabled');
+            // 显示加载中文本
+            document.getElementById('loadingText').hidden = false;
+            // 隐藏表格
+            document.getElementById('pc_table').hidden = true;
+            document.getElementById('mobile_table').hidden = true;
+            // 获取数据
             axios.post('/api/timeattack', {
                 course: document.getElementById('floatingSelectGrid_Course').value,
                 carModel: document.getElementById('floatingSelectGrid_CarModel').value
             }).then((response) => {
                 // 如果没有数据则返回
                 if (response.data.length === 0) {
-                    this.leaderboard = [
-                        {
-                            fetchTime: '暂无数据',
-                            car: {
-                                name: '暂无数据',
-                            },
-                            totalTime: '暂无数据',
-                            controlLevel: '暂无数据'
-                        }
-                    ];
-                    return;
+                    this.leaderboard = [];
+                    document.getElementById('loadingText').innerHTML = '暂无数据';
                 } else {
-                    console.log(response.data);
                     this.leaderboard = response.data || [];
                     this.changePage(1);
                 }
             }).catch((error) => {
                 console.log(error);
-            });
+            })
+                .finally(() => {
+                    // 恢复下拉框
+                    document.getElementById('floatingSelectGrid_Course').removeAttribute('disabled');
+                    document.getElementById('floatingSelectGrid_CarModel').removeAttribute('disabled');
+                    if (this.leaderboard.length === 0) {
+                        document.getElementById('loadingText').innerHTML = '暂无数据';
+                        document.getElementById('loadingText').hidden = false;
+                    } else {
+                        document.getElementById('loadingText').innerHTML = '加载数据中......请稍候';
+                        document.getElementById('loadingText').hidden = true;
+                    }
+                    // 显示表格
+                    document.getElementById('pc_table').hidden = false;
+                    document.getElementById('mobile_table').hidden = false;
+                });
         },
         getRowClass(index) {
             if (index === 0 && this.currentPage === 1) {
@@ -210,6 +290,15 @@ export default {
 </script>
 
 <style scoped>
+.lg-vp-ul-fix {
+    padding-left: 0 !important;
+    margin: 0 !important;
+}
+
+.lg-vp-li-fix {
+    margin-top: 0 !important;
+}
+
 .gold {
     color: black;
     background: linear-gradient(to bottom, rgba(252, 236, 114, 1) 0%, rgba(255, 239, 117, 1) 0%, rgba(251, 239, 119, 1) 1%, rgba(255, 240, 125, 1) 2%, rgba(254, 239, 120, 1) 4%, rgba(255, 240, 121, 1) 6%, rgba(255, 246, 145, 1) 27%, rgba(255, 246, 143, 1) 30%, rgba(255, 247, 149, 1) 33%, rgba(253, 248, 154, 1) 33%, rgba(254, 248, 162, 1) 38%, rgba(255, 248, 167, 1) 39%, rgba(255, 252, 175, 1) 44%, rgba(255, 250, 169, 1) 45%, rgba(254, 250, 176, 1) 46%, rgba(252, 252, 178, 1) 47%, rgba(255, 251, 170, 1) 48%, rgba(255, 251, 170, 1) 48%, rgba(254, 239, 146, 1) 49%, rgba(250, 231, 139, 1) 49%, rgba(255, 227, 128, 1) 50%, rgba(255, 230, 130, 1) 50%, rgba(255, 228, 125, 1) 51%, rgba(255, 227, 128, 1) 52%, rgba(252, 224, 124, 1) 52%, rgba(255, 224, 123, 1) 53%, rgba(255, 213, 111, 1) 60%, rgba(255, 214, 106, 1) 60%, rgba(254, 211, 99, 1) 63%, rgba(255, 208, 90, 1) 64%, rgba(255, 208, 90, 1) 66%, rgba(255, 208, 84, 1) 66%, rgba(254, 204, 81, 1) 67%, rgba(227, 179, 55, 1) 83%, rgba(222, 174, 50, 1) 85%, rgba(223, 175, 51, 1) 86%, rgba(217, 169, 45, 1) 88%, rgba(208, 163, 38, 1) 94%, rgba(207, 161, 41, 1) 97%, rgba(205, 159, 39, 1) 99%, rgba(197, 159, 58, 1) 100%, rgba(185, 147, 46, 1) 100%);
