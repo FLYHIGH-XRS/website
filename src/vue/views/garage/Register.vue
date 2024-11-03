3024990936a90217b2ecd85c9a6ef87045357d2a
kKsk <alonzomax1210@gmail.com>
2024-11-03 22:20:30 +0800

    fix register

--- src/vue/views/garage/Register.vue
+++ src/vue/views/garage/Register.vue
@@ -74,6 +74,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 import * as validityCheck from "../../static/js/validityCheck";
